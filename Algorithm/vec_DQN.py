66a92fa0f14e0b2b146caf2f2656d27d110dacbd
JLu2022 <junlinlu16@gmail.com>
2023-10-24 17:27:41 +0100

    further tune neededk

--- Algorithm/vec_DQN.py
+++ Algorithm/vec_DQN.py
@@ -16,81 +16,6 @@ from simulators.deep_sea_treasure.deep_sea_treasure import DeepSeaTreasure
 
 # from simulators.minecart.minecart_simulator import Minecart
 
-"""GPI-PD algorithm."""
-import os
-import random
-from itertools import chain
-from typing import Callable, List, Optional, Union
-
-import gymnasium as gym
-import numpy as np
-import torch as th
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import wandb
-
-from common.buffer import ReplayBuffer
-from common.evaluation import (
-    log_all_multi_policy_metrics,
-    log_episode_info,
-    policy_evaluation_mo,
-)
-from common.model_based.probabilistic_ensemble import (
-    ProbabilisticEnsemble,
-)
-from common.model_based.utils import ModelEnv, visualize_eval
-from common.morl_algorithm import MOAgent, MOPolicy
-from common.networks import (
-    NatureCNN,
-    get_grad_norm,
-    huber,
-    layer_init,
-    mlp,
-    polyak_update,
-)
-from common.prioritized_buffer import PrioritizedReplayBuffer
-from common.utils import linearly_decaying_value, unique_tol
-from common.weights import equally_spaced_weights
-from linear_support import LinearSupport
-
-
-class QNet(nn.Module):
-    """Conditioned MO Q network."""
-
-    def __init__(self, obs_shape, action_dim, rew_dim, net_arch, drop_rate=0.01, layer_norm=True):
-        """Initialize the net.
-
-        Args:
-            obs_shape: The observation shape.
-            action_dim: The action dimension.
-            rew_dim: The reward dimension.
-            net_arch: The network architecture.
-            drop_rate: The dropout rate.
-            layer_norm: Whether to use layer normalization.
-        """
-        super().__init__()
-        self.obs_shape = obs_shape
-        self.action_dim = action_dim
-        self.phi_dim = rew_dim
-
-        self.weights_features = mlp(rew_dim, -1, net_arch[:1])
-        if len(obs_shape) == 1:
-            self.state_features = mlp(obs_shape[0], -1, net_arch[:1])
-        elif len(obs_shape) > 1:  # Image observation
-            self.state_features = NatureCNN(self.obs_shape, features_dim=net_arch[0])
-        self.net = mlp(
-            net_arch[0], action_dim * rew_dim, net_arch[1:], drop_rate=drop_rate, layer_norm=layer_norm
-        )  # 128/128 256 256 256
-
-        self.apply(layer_init)
-
-    def forward(self, obs, w):
-        """Forward pass."""
-        sf = self.state_features(obs)
-        wf = self.weights_features(w)
-        q_values = self.net(sf * wf)
-        return q_values.view(-1, self.action_dim, self.phi_dim)  # Batch size X Actions X Rewards
 
 class PreferenceSpace:
     def __init__(self, fixed_w=None):
@@ -131,7 +56,7 @@ class ConditionedDQNAgent:
 
         self.input_size = self.env.observation_space
         self.weight_size = reward_dim
-        self.output_size = self.env.action_space * reward_dim
+        self.output_size = self.env.action_space
 
         self.model = self.build_model()
         self.target_model = self.build_model()
@@ -157,9 +82,9 @@ class ConditionedDQNAgent:
 
         # Build full model
         model = keras.Model(inputs=[state_input, weight_input], outputs=outputs)
-        optimizer = keras.optimizers.Adam(learning_rate=1e-3)
-        loss_fn = keras.losses.mean_squared_error
-        model.compile(optimizer=optimizer, loss=loss_fn)
+        self.optimizer = keras.optimizers.Adam(learning_rate=1e-3)
+        self.loss_fn = keras.losses.mean_squared_error
+        model.compile(optimizer=self.optimizer, loss=self.loss_fn)
         return model
 
     def epsilon_greedy_policy(self, state, weights, epsilon, evaluation=False):
@@ -167,9 +92,8 @@ class ConditionedDQNAgent:
             return random.choice(self.actions)
         else:
             Q_values = self.model([state[np.newaxis], weights[np.newaxis]], training=False)
-            Q_values = tf.reshape(Q_values, (-1, 2))
             # print(f"Q_values:{np.dot(Q_values, weights)}")
-            action = np.argmax(np.dot(Q_values, weights))
+            action = np.argmax(Q_values)
             if evaluation:
                 print(f"state:{np.round_(state, 3)}\tQ:{np.round_(Q_values, 3)}\taction:{action}")
             return action
@@ -179,19 +103,12 @@ class ConditionedDQNAgent:
         return action
 
     def update(self, tensor_w):
-        tensor_w = tf.reshape(tensor_w,(1,2,1))
         experiences = self.replay_memory.sample(self.batch_size)
         states, actions, rewards, next_states, dones, weightss = experiences
 
         # Compute target Q values from 'next_states'
         next_Q_values = self.target_model([next_states, weightss], training=False)
-        print(f"next_Q:{next_Q_values}")
-        next_Q_values = tf.reshape(next_Q_values, (-1, 2, 4))
-        print(f"next_Q:{next_Q_values}")
-        next_Q_values = tf.reduce_sum(next_Q_values, tensor_w)
-        print(f"next_Q:{next_Q_values}")
         max_next_Q_values = np.max(next_Q_values, axis=1)
-        print(f"rewards:{rewards}\tmax_Q:{max_next_Q_values}")
         target_Q_values = (rewards + (1 - dones) * self.gamma * max_next_Q_values)
         target_Q_values = target_Q_values.reshape(-1, 1)  # Make column vector
 
@@ -202,10 +119,10 @@ class ConditionedDQNAgent:
             # print(f"states:{states}\nweightss:{weightss}")
             all_Q_values = self.model([states, weightss])
             Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
-            q_loss = tf.reduce_mean(self.model.loss_fn(target_Q_values, Q_values))
+            q_loss = tf.reduce_mean(self.loss_fn(target_Q_values, Q_values))
         # print(f"q_loss:{q_loss}")
         grads = tape.gradient(q_loss, self.model.trainable_variables)
-        self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
 
     def evaluate_demo(self, demo, eval_env, weights=np.array([1, 0])):
         disc_vec_return = np.zeros_like(weights, dtype=np.float64)
@@ -238,13 +155,23 @@ class ConditionedDQNAgent:
 
         EU_target = np.mean(utility_thresholds)
         EU = -np.inf
-        while EU < EU_target:
+        iterations = 0
+        while EU < EU_target * 0.95:
+            iterations += 1
             self.jsmorl_train_iteration(eval_env=eval_env, eval_freq=100, corners=corners, demos=demos)
             utilities = []
             for w in corners:
-                utility, disc_vec_return = self.play_a_episode(env=eval_env, agent=self, demo=[], weights=w)
+                utility, disc_vec_return, _, _ = self.play_a_episode(env=eval_env, agent=self, demo=[], weights=w)
                 utilities.append(utility)
             EU = np.mean(utilities)
+            print(f"@iteration{iterations}-- EU:{EU}\tEU_target:{EU_target}")
+        print(f"reach!!!!@step:{self.global_step}")
+        self.model.save(filepath="abc_model")
+        corners = sorted(corners, key=lambda x: x[0])
+        for w in corners:
+            disc_return, disc_vec_return, scalar_return, vec_return = self.play_a_episode(env=eval_env, weights=w,
+                                                                                          agent=self, demo=[])
+            print(f"for w:{np.round_(w,3)}\tdisc vec return:{vec_return}")
 
     def jsmorl_train_iteration(self,
                                eval_env=None,
@@ -277,9 +204,10 @@ class ConditionedDQNAgent:
                 action = self._act(state=obs, weights=w, epsilon=0.5)
 
             next_obs, vec_reward, terminated, truncated, info = self.env.step(action)
+            reward = np.dot(vec_reward, w)
             next_obs = np.array(next_obs)
 
-            self.replay_memory.append([obs, action, vec_reward, next_obs, terminated, w])
+            self.replay_memory.append([obs, action, reward, next_obs, terminated, w])
 
             if self.global_step >= self.start_training_after:
                 self.update(tf.convert_to_tensor(w))
@@ -287,67 +215,19 @@ class ConditionedDQNAgent:
                     self.target_model.set_weights(self.model.get_weights())
 
             if self.global_step % eval_freq == 0:
-                u, disc_vec_return = self.play_a_episode(env=eval_env, weights=w, agent=self, demo=demo[:pi_g_horizon])
+                u, disc_vec_return,_,_ = self.play_a_episode(env=eval_env, weights=w, agent=self, demo=demo[:pi_g_horizon])
                 if u >= utility_threshold:
                     print(f"@:{w} -- reach threshold")
                     pi_g_horizon -= 2
                     pi_g_horizon = max(pi_g_horizon, 0)
 
-                print(f"guide policy horizon:{pi_g_horizon}\t demo:{demos}\tw_c:{corners}")
+                print(f"guide policy horizon:{pi_g_horizon}\t demo:{demo}\tw_c:{w}")
 
             if terminated or truncated:
                 obs, _ = self.env.reset()
                 obs = np.array(obs)
             else:
                 obs = next_obs
-
-    # def train_model(self, steps, save_per=100000, show_detail_per=1000, pref_space=PreferenceSpace(),
-    #                 corner_weights=None, eval_env=None):
-    #     """
-    #     Train the network over a range of episodes.
-    #     """
-    #
-    #     for step in range(1, steps + 1):
-    #         self.action_list = []
-    #         eps = 0.5
-    #         # Reset env
-    #         state, _ = self.env.reset()
-    #         state = np.array(state)  # Convert to float32 for tf
-    #
-    #         episode_reward = 0
-    #         rewards_vec = np.zeros(3)
-    #         # weights = pref_space.sample()
-    #         weights = np.array([0.34, 0.36, 0.3])
-    #         while True:
-    #             state, reward, done, rewards = self.play_one_step(np.array(state), eps, weights)
-    #             steps += 1
-    #             episode_reward += reward
-    #             rewards_vec += rewards
-    #             if done:
-    #                 break
-    #         if step > self.start_training_after:  # Wait for buffer to fill up a bit
-    #             self.training_step()
-    #             if step % self.copy_to_target_per == 0:
-    #                 self.target_model.set_weights(self.model.get_weights())
-    #             if step % save_per == 0 and step >= save_per and self.checkpoint:
-    #                 self.model.save(self.model_path + str(step))
-    #             u = self.play_a_episode(env=eval_env, weights=np.array([0, 1]), agent=self, demo=[])
-    #             print(f"tr"
-    #                   f"yout_u:{u}")
-    #         if step % show_detail_per == 0:
-    #             if sum(self.action_list) > 0:
-    #                 indices_of_one = [i for i, x in enumerate(self.action_list) if x == 1]
-    #             else:
-    #                 indices_of_one = "Not run at all"
-    #             print(f"Epoch:{step}\t"
-    #                   f"Pref:{weights}"
-    #                   f"Epoch Reward/Cost:{episode_reward}\t"
-    #                   f"Reward Vec:{rewards_vec}\t"
-    #                   f"Epsilon:{np.round(eps, 2)}\t"
-    #                   f"Actions:{sum(self.action_list)}\t"
-    #                   f"@{indices_of_one}")
-    #     # u = self.play_a_episode(env=eval_env, pref_w=np.array([0., 1.]), agent=self, demo=[])
-    #     # print(f"utility:{u}")
 
     def weight_to_demo(self, w, demos):
         disc_scalar_returns = []
@@ -364,9 +244,11 @@ class ConditionedDQNAgent:
         max_vec_return = vec_returns[max_demo_idx]
         return max_demo, max_utility, max_vec_return
 
-    def play_a_episode(self, env, weights, agent, demo):
+    def play_a_episode(self, env, weights, agent, demo, evaluation=False):
         disc_vec_return = np.zeros_like(weights, dtype=np.float64)
+        vec_return = np.zeros_like(weights, dtype=np.float64)
         disc_return = 0
+        scalar_return = 0
         gamma = 1
         terminal = False
         state, _ = env.reset()
@@ -381,19 +263,21 @@ class ConditionedDQNAgent:
                 action = demo[action_pointer]
                 action_pointer += 1
             else:
-                action = agent.epsilon_greedy_policy(state, weights=weights, epsilon=0, evaluation=True)
+                action = agent.epsilon_greedy_policy(state, weights=weights, epsilon=0, evaluation=evaluation)
             traj_actions.append(action)
             n_state, rewards, terminal, _, _ = env.step(action)
             n_state = np.array(n_state)
             traj_states.append(n_state)
             disc_vec_return += gamma * rewards
+            vec_return += rewards
             disc_return += gamma * np.dot(rewards, weights)
+            scalar_return = np.dot(rewards, weights)
             gamma *= self.gamma
             state = n_state
             if steps > 100:
                 break
-        print(f"eval action traj:{traj_actions}")
-        return disc_return, disc_vec_return
+        # print(f"eval action traj:{traj_actions}")
+        return disc_return, disc_vec_return, scalar_return, vec_return
 
 
 if __name__ == '__main__':
